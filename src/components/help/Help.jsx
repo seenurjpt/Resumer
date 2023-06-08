ff1d19846002867924e986f3a9aa84a4fde03d41
seenurjpt <seenurjpt1402@gmail.com>
2023-06-08 09:39:16 +0530

    C6

--- src/components/help/Help.jsx
+++ src/components/help/Help.jsx
@@ -8,7 +8,6 @@ const Help = () => {
       <div>
         <ol className='list-decimal'>
             <li>It is simple to use, Just fill your details with editor and click on "Save" Button. Your changes with details will reflect in the resume preview below the editor.</li>
-            {/* <p>Your changes with details will reflect in the resume preview below the editor.</p> */}
             <li>You can change the placing of any section by just doing simple drag and drop.</li>
             <li>You can change colors by selecting color from color pallete.</li>
             <li>If you want to delete a section, Just empty the title of that section then click "Save".</li>
